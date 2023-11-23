7359053a1637a8570f185106b93051768733e51b
maryam16mgh <mmgh16@gmail.com>
2023-11-23 09:27:57 +0300

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Maryam Mohammad on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
